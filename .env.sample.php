6e19d752abcbdab7554fdd58e5c4548649c2efee
xjchen <xchenxujian@126.com>
2015-02-05 15:14:37 +0000

    first commit

---
+++ .env.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'DB_HOST' => 'localhost', //数据库地址
+    'DB_PORT' => '3306', //数据库端口
+    'DB_NAME' => 'homestead', //数据库名称
+    'DB_USER' => 'homestead', //数据库账号
+    'DB_PASSWORD' => 'secret', //数据库密码
+    'DB_PREFIX' => '', //数据库表前缀
+];
---
+++ readme.md
@@ -0,0 +1,3 @@
+Huying BasicAdmin Template
+==========================
+
