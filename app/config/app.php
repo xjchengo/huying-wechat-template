089126d49c9e5c99762053af2d0c16eba09d749a
xjchen <xchenxujian@126.com>
2015-02-06 01:31:10 +0000

    comment BasicAdminServiceProvider

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		'Xjchen\BasicAdmin\BasicAdminServiceProvider',
+		// 'Xjchen\BasicAdmin\BasicAdminServiceProvider',
 
 	),
 
