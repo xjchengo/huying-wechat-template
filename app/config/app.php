57c68d81c9fb7959949bf0249675f41908c309ee
xjchen <xchenxujian@126.com>
2015-02-07 07:52:47 +0000

    add debug info

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		'Xjchen\BasicAdmin\BasicAdminServiceProvider',
+		//weihuying 'Xjchen\BasicAdmin\BasicAdminServiceProvider',
 
 	),
 
